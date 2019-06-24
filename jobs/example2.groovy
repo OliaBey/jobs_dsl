0b12263f908a415a5c93e58d2c70ceea04ae7e7f
OliaBey <43787985+OliaBey@users.noreply.github.com>
2019-06-24 09:10:22 +0300

    Update example2.groovy

--- jobs/example2.groovy
+++ jobs/example2.groovy
@@ -1,9 +1,8 @@
-configFiles {
-    customConfig {
-        id('example2')
-        name('Example from directory JOBS')
-        comment('')
-        content(this.readFileFromWorkspace('jobs/example2.pipeline'))
+pipelineJob('example2') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('jobs/example2.pipeline'))
+            sandbox()
+        }
     }
 }
-
