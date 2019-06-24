25f2224d698c59da345df7f7f9bd69a21fb91106
OliaBey <43787985+OliaBey@users.noreply.github.com>
2019-06-24 09:24:50 +0300

    Update example2.groovy

--- jobs/example2.groovy
+++ jobs/example2.groovy
@@ -6,3 +6,11 @@ pipelineJob('example2') {
         }
     }
 }
+configFiles {
+    customConfig {
+        id('example2')
+        name('Example from directory JOBS')
+        comment('')
+        content(this.readFileFromWorkspace('jobs/example2.pipeline'))
+    }
+}
